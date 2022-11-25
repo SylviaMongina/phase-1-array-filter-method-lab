e5051043491c731076df78a8250b8fa6e5cf63ac
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2022-11-25 18:31:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driver, name) {
+    return driver.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+  function fuzzyMatch(name, testString) {
+    return name.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(driver, driverName) {
+    return driver.filter((record) => record.name === driverName);
+  }
